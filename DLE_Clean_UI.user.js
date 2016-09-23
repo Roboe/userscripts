ffd00f2f282393969381893384f7f03698e139c4
Roboe <roboe@virgulilla.com>
2016-09-23 13:45:41 +0200

    DLE: Upgrade version to 1.1

--- DLE_Clean_UI.user.js
+++ DLE_Clean_UI.user.js
@@ -8,7 +8,7 @@
 // @updateURL   https://gitlab.com/Roboe/userscripts/raw/master/DLE_Clean_UI.user.js
 // @icon        https://gitlab.com/Roboe/userscripts/raw/master/img/dle_icon.jpg
 // @license     GPLv3; http://www.gnu.org/licenses/gpl.html
-// @version     1.0
+// @version     1.1
 // @include     http://dle.rae.es/*
 // @run-at      document-end
 // @grant       GM_addStyle
