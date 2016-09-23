99bc4cc33f62318f5cdcf3604df9a3fa9d6b88a4
Roboe <roboe@virgulilla.com>
2016-09-23 13:24:30 +0200

    DLE: Use GreaseMonkey's @run-at
    Make use of the GreaseMonkey's declarative lifecycle instead of manually catching the DOMContentLoaded event

--- DLE_Clean_UI.user.js
+++ DLE_Clean_UI.user.js
@@ -10,27 +10,26 @@
 // @license     GPLv3; http://www.gnu.org/licenses/gpl.html
 // @version     1.0
 // @include     http://dle.rae.es/*
+// @run-at      document-end
 // @grant       GM_addStyle
 // ==/UserScript==
 
-window.addEventListener('DOMContentLoaded', function() {
-  GM_addStyle('/* Injected by userscript */\
-  #idle { height: 50px; }\
-  #drea23 { padding-top: 1.4em; }\
-  #irae { margin: 1.2em 0 0 1em; height: unset; width: unset; }\
-  #iasale { margin: 1em 1em 0; height: unset; width: unset; }\
-  #resultados {margin-top: 4.5em; }\
-  @media all and (min-width: 737px) {\
-    #resultados { margin-top: 0; }\
-  }\
-  @media all and (min-width: 841px) {\
-    #irae { margin: .5em 1em; height: 65px; width: auto; }\
-    #iasale { margin: 0 1em; height: 100px; width: auto; }\
-  }\
-  #header { display: none; /*top: 120px;*/ }\
-  body.dle #main { margin-top: -12em !important; }\
-  #diccionario { margin: 0 auto; width: unset; }\
-  #informa { display: none; }\
-  .box { padding-bottom: 0 !important; }\
-  ');
-}, false);
+GM_addStyle('/* Injected by userscript */\
+#idle { height: 50px; }\
+#drea23 { padding-top: 1.4em; }\
+#irae { margin: 1.2em 0 0 1em; height: unset; width: unset; }\
+#iasale { margin: 1em 1em 0; height: unset; width: unset; }\
+#resultados {margin-top: 4.5em; }\
+@media all and (min-width: 737px) {\
+  #resultados { margin-top: 0; }\
+}\
+@media all and (min-width: 841px) {\
+  #irae { margin: .5em 1em; height: 65px; width: auto; }\
+  #iasale { margin: 0 1em; height: 100px; width: auto; }\
+}\
+#header { display: none; /*top: 120px;*/ }\
+body.dle #main { margin-top: -12em !important; }\
+#diccionario { margin: 0 auto; width: unset; }\
+#informa { display: none; }\
+.box { padding-bottom: 0 !important; }\
+');
